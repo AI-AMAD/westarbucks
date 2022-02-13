719eadedf0b51fdc9f824991cbcef4a719e6faf7
천지은 <jieun@MacBook-Pro-5.local>
2022-02-13 16:57:16 +0900

    CREATE: 프로젝트 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'Ljcje0325!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+
+SECRET_KEY = 'django-insecure-opufp)sv#!a%aq*x9!e7gek-3&f1&qri)i_s9j1ovdfwffn(bb'
